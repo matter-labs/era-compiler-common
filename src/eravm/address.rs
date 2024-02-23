2601ea01b286531bf89ee7335f8acb298595bb16
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-02-23 22:42:15 +0100

    Add simulations for forwarding returns

--- src/eravm/address.rs
+++ src/eravm/address.rs
@@ -109,3 +109,9 @@ pub const ERAVM_ADDRESS_DECOMMIT: u16 = 0xFFDD;
 
 /// The corresponding simulation predefined address.
 pub const ERAVM_ADDRESS_ACTIVE_PTR_LOAD_DECOMMIT: u16 = 0xFFDC;
+
+/// The corresponding simulation predefined address.
+pub const ERAVM_ADDRESS_RETURN_FORWARD: u16 = 0xFFDB;
+
+/// The corresponding simulation predefined address.
+pub const ERAVM_ADDRESS_REVERT_FORWARD: u16 = 0xFFDA;
