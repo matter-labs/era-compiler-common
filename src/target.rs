e1cf94f275b4bac66f0341dec60508226e962a3c
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-08-22 18:12:13 +0800

    fix: add serde to Target

--- src/target.rs
+++ src/target.rs
@@ -7,7 +7,7 @@ use std::str::FromStr;
 ///
 /// The LLVM target.
 ///
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub enum Target {
     /// The EraVM target.
     EraVM,
