1294be950365c9cfb679b23641afe9cf52fedeb4
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-10-26 22:41:04 +0800

    fix: downloading on Windows via compiler bin JSON

--- era-compiler-downloader/src/lib.rs
+++ era-compiler-downloader/src/lib.rs
@@ -1,4 +1,8 @@
-mod config;
+//!
+//! The compiler downloader config.
+//!
+
+pub(crate) mod config;
 
 #[cfg(target_family = "unix")]
 use std::os::unix::fs::PermissionsExt;
@@ -40,79 +44,65 @@ impl Downloader {
     ///
     pub fn download(mut self, config_path: &Path) -> anyhow::Result<Config> {
         let config_file = std::fs::File::open(config_path).map_err(|error| {
-            anyhow::anyhow!(
-                "Binaries download config {:?} opening error: {}",
-                config_path,
-                error
-            )
+            anyhow::anyhow!("Executable downloader config {config_path:?} opening error: {error}")
         })?;
         let config_reader = std::io::BufReader::new(config_file);
         let config: Config = serde_json::from_reader(config_reader).map_err(|error| {
-            anyhow::anyhow!(
-                "Binaries download config {:?} parsing error: {}",
-                config_path,
-                error
-            )
+            anyhow::anyhow!("Executable downloader config {config_path:?} parsing error: {error}")
         })?;
 
         let platform_directory = config.get_remote_platform_directory()?;
 
-        for (version, executable) in config.binaries.iter() {
+        for (version, executable) in config.executables.iter() {
             if !executable.is_enabled {
                 continue;
             }
 
-            let source_path = executable
+            let mut source_path = executable
                 .source
                 .replace("${PLATFORM}", platform_directory.as_str())
-                .replace("${VERSION}", version.as_str())
-                + (std::env::consts::EXE_SUFFIX);
+                .replace("${VERSION}", version.as_str());
 
             let destination_path = executable
                 .destination
                 .replace("${VERSION}", version.as_str());
             let destination_path = PathBuf::from_str(
-                format!("{}{}", destination_path, std::env::consts::EXE_SUFFIX).as_str(),
+                format!("{destination_path}{}", std::env::consts::EXE_SUFFIX).as_str(),
             )
             .map_err(|_| {
-                anyhow::anyhow!("Executable `{}` destination is invalid", destination_path)
+                anyhow::anyhow!("Executable `{destination_path}` destination is invalid")
             })?;
 
             let data = match executable.protocol {
                 Protocol::File => {
+                    source_path += std::env::consts::EXE_SUFFIX;
                     if source_path == destination_path.to_string_lossy() {
                         println!(
-                            "    {} executable {:?}. The source and destination are the same.",
+                            "    {} executable {destination_path:?}. The source and destination are the same.",
                             "Skipping".bright_green().bold(),
-                            destination_path,
                         );
                         continue;
                     }
 
                     println!(
-                        "     {} executable `{}` => {:?}",
+                        "     {} executable `{source_path}` => {destination_path:?}",
                         "Copying".bright_green().bold(),
-                        source_path,
-                        destination_path,
                     );
 
                     std::fs::copy(source_path.as_str(), executable.destination.as_str()).map_err(
                         |error| {
-                            anyhow::anyhow!(
-                                "Executable {:?} copying error: {}",
-                                source_path.as_str(),
-                                error
-                            )
+                            anyhow::anyhow!("Executable {source_path:?} copying error: {error}",)
                         },
                     )?;
                     continue;
                 }
                 Protocol::HTTPS => {
+                    source_path += std::env::consts::EXE_SUFFIX;
+
                     if destination_path.exists() {
                         println!(
-                            "    {} executable {:?}. Already exists.",
+                            "    {} executable {destination_path:?}. Already exists.",
                             "Skipping".bright_green().bold(),
-                            destination_path,
                         );
                         continue;
                     }
@@ -120,19 +110,16 @@ impl Downloader {
                     let source_url =
                         reqwest::Url::from_str(source_path.as_str()).expect("Always valid");
                     println!(
-                        " {} executable `{}` => {:?}",
+                        " {} executable `{source_url}` => {destination_path:?}",
                         "Downloading".bright_green().bold(),
-                        source_url,
-                        destination_path,
                     );
                     self.http_client.get(source_url).send()?.bytes()?
                 }
                 Protocol::CompilerBinList => {
                     if destination_path.exists() {
                         println!(
-                            "    {} executable {:?}. Already exists.",
+                            "    {} executable {destination_path:?}. Already exists.",
                             "Skipping".bright_green().bold(),
-                            destination_path,
                         );
                         continue;
                     }
@@ -150,10 +137,17 @@ impl Downloader {
                         match compiler_list.releases.get(version.to_string().as_str()) {
                             Some(source_executable_name) => source_executable_name,
                             None => anyhow::bail!(
-                                "Executable for version v{} not found in the compiler JSON list",
-                                version
-                            ),
+                            "Executable for version v{version} not found in the compiler JSON list",
+                        ),
                         };
+                    #[cfg(target_os = "windows")]
+                    if !source_executable_name.ends_with(std::env::consts::EXE_SUFFIX) {
+                        println!(
+                            "    {} downloading {source_executable_name:?}. Not an executable file.",
+                            "Skipping".bright_green().bold(),
+                        );
+                        continue;
+                    }
                     let mut source_path = compiler_list_path;
                     source_path.pop();
                     source_path.push(source_executable_name);
@@ -162,10 +156,8 @@ impl Downloader {
                         reqwest::Url::from_str(source_path.to_str().expect("Always valid"))
                             .expect("Always valid");
                     println!(
-                        " {} executable `{}` => {:?}",
+                        " {} executable `{source_url}` => {destination_path:?}",
                         "Downloading".bright_green().bold(),
-                        source_url,
-                        destination_path,
                     );
                     self.http_client.get(source_url).send()?.bytes()?
                 }
