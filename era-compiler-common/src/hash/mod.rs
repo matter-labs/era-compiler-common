9bafeabc66d33694409208c51749634a86c1772a
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2025-04-05 14:21:43 +0800

    feat: make IPFS hash and CBOR equivalent again (#40)

--- era-compiler-common/src/hash/mod.rs
+++ era-compiler-common/src/hash/mod.rs
@@ -1,153 +1,54 @@
 //!
-//! Hashing utilities.
+//! Hash utilities.
 //!
 
-pub mod r#type;
+pub mod ipfs;
+pub mod keccak256;
 
-use base58::FromBase58;
-use sha3::digest::FixedOutput;
-use sha3::Digest;
+use self::ipfs::IPFS;
+use self::keccak256::Keccak256;
 
 ///
-/// Hash types supported so far.
+/// Hash enum to make encoding easier.
 ///
 #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 pub enum Hash {
-    /// The `keccak256` hash.
-    Keccak256 {
-        /// The byte representation.
-        bytes: [u8; crate::BYTE_LENGTH_FIELD],
-        /// The string representation.
-        string: String,
-    },
-    /// The `ipfs` hash.
-    Ipfs {
-        /// The byte representation.
-        #[serde(with = "serde_arrays")]
-        bytes: [u8; 2 + crate::BYTE_LENGTH_FIELD],
-        /// The string representation.
-        string: String,
-    },
+    /// IPFS hash.
+    IPFS(IPFS),
+    /// Keccak256 hash.
+    Keccak256(Keccak256),
 }
 
 impl Hash {
     ///
-    /// Computes the `keccak256` hash for `preimage`.
-    ///
-    pub fn keccak256(preimage: &[u8]) -> Self {
-        let bytes = sha3::Keccak256::digest(preimage).into();
-        let string = format!("0x{}", hex::encode(bytes));
-        Self::Keccak256 { bytes, string }
-    }
-
-    ///
-    /// Computes the `keccak256` hash for an array of `preimages`.
-    ///
-    pub fn keccak256_multiple<R: AsRef<[u8]>>(preimages: &[R]) -> Self {
-        let mut hasher = sha3::Keccak256::new();
-        for preimage in preimages.iter() {
-            hasher.update(preimage);
-        }
-        let bytes: [u8; crate::BYTE_LENGTH_FIELD] = hasher.finalize_fixed().into();
-        let string = format!("0x{}", hex::encode(bytes));
-        Self::Keccak256 { bytes, string }
-    }
-
-    ///
-    /// Computes the `ipfs` hash for `preimage`.
-    ///
-    pub fn ipfs(preimage: &[u8]) -> Self {
-        let hasher = ipfs_hasher::IpfsHasher::default();
-        let string = hasher.compute(preimage);
-        let bytes = string
-            .from_base58()
-            .expect("Base58 conversion is always valid")
-            .try_into()
-            .expect("The size is always correct");
-        Self::Ipfs { bytes, string }
-    }
-
-    ///
-    /// Formats a hash array to a hexadecimal string.
+    /// Returns a reference to the hash bytes.
     ///
     pub fn as_bytes(&self) -> &[u8] {
         match self {
-            Self::Keccak256 { bytes, .. } => bytes.as_slice(),
-            Self::Ipfs { bytes, .. } => bytes.as_slice(),
+            Hash::IPFS(hash) => hash.as_bytes(),
+            Hash::Keccak256(hash) => hash.as_bytes(),
         }
     }
 
     ///
-    /// Converts the hash into a CBOR-encoded byte array.
+    /// Returns a reference to the hash string.
     ///
-    pub fn as_cbor_bytes(&self) -> Vec<u8> {
-        let data = match self {
-            Self::Ipfs { bytes, .. } => bytes.as_slice(),
-            Self::Keccak256 { .. } => panic!("Only IPFS hash can be CBOR-encoded"),
-        };
-
-        let mut cbor = Vec::with_capacity(64);
-        cbor.extend(hex::decode("a1646970667358").expect("Always valid"));
-        cbor.push(data.len() as u8);
-        cbor.extend_from_slice(data);
-        cbor.extend((cbor.len() as u16).to_be_bytes());
-        cbor
-    }
-}
-
-impl std::fmt::Display for Hash {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    pub fn as_str(&self) -> &str {
         match self {
-            Self::Keccak256 { string, .. } => write!(f, "{string}"),
-            Self::Ipfs { string, .. } => write!(f, "{string}"),
+            Hash::IPFS(hash) => hash.as_hex_str(),
+            Hash::Keccak256(hash) => hash.as_str(),
         }
     }
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn keccak256() {
-        assert_eq!(
-            super::Hash::keccak256("zksync".as_bytes()).to_string(),
-            "0x0238fb1ab06c28c32885f9a4842207ac480c2467df26b6c58e201679628c5a5b"
-        );
+impl From<IPFS> for Hash {
+    fn from(hash: IPFS) -> Self {
+        Self::IPFS(hash)
     }
+}
 
-    #[test]
-    fn keccak256_multiple() {
-        assert_eq!(
-            super::Hash::keccak256_multiple(&[
-                "zksync".as_bytes(),
-                "the".as_bytes(),
-                "best".as_bytes()
-            ])
-            .to_string(),
-            "0x30277e6e189b3fa474437d451ccbb2409c3b67fda53c6ad5df3f8f0f3873ff6b"
-        );
-    }
-
-    #[test]
-    fn ipfs() {
-        assert_eq!(
-            super::Hash::ipfs("zksync".as_bytes()).to_string(),
-            "QmZNRNrU4GknvaB3kMuH2tMjw2ji9fCmeVA9R6yPboiF4J"
-        );
-    }
-
-    #[test]
-    fn ipfs_hex() {
-        assert_eq!(
-            hex::encode(super::Hash::ipfs("zksync".as_bytes()).as_bytes()),
-            "1220a3e4a4b11362b17e7294afb3048e97a6cb024209f236d7388ef55476192413bf"
-        );
-    }
-
-    #[test]
-    fn ipfs_cbor() {
-        assert_eq!(
-            hex::encode(super::Hash::ipfs("zksync".as_bytes()).as_cbor_bytes()),
-            "a1646970667358221220a3e4a4b11362b17e7294afb3048e97a6cb024209f236d7388ef55476192413bf002a"
-        );
+impl From<Keccak256> for Hash {
+    fn from(hash: Keccak256) -> Self {
+        Self::Keccak256(hash)
     }
 }
--- era-compiler-common/src/metadata_hash_type/evm.rs
+++ era-compiler-common/src/metadata_hash_type/evm.rs
@@ -1,23 +1,20 @@
 //!
-//! The hash type.
+//! EVM metadata hash type.
 //!
 
 use std::str::FromStr;
 
 ///
-/// The hash type.
+/// EVM metadata  hash type.
 ///
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
 pub enum Type {
     /// Do not include bytecode hash.
     #[serde(rename = "none")]
     None,
-    /// The `keccak256`` hash type.
-    #[serde(rename = "keccak256")]
-    Keccak256,
     /// The `ipfs` hash.
     #[serde(rename = "ipfs")]
-    Ipfs,
+    IPFS,
 }
 
 impl FromStr for Type {
@@ -26,8 +23,7 @@ impl FromStr for Type {
     fn from_str(string: &str) -> Result<Self, Self::Err> {
         match string {
             "none" => Ok(Self::None),
-            "keccak256" => Ok(Self::Keccak256),
-            "ipfs" => Ok(Self::Ipfs),
+            "ipfs" => Ok(Self::IPFS),
             string => anyhow::bail!("unknown bytecode hash mode: `{string}`"),
         }
     }
@@ -37,8 +33,7 @@ impl std::fmt::Display for Type {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             Self::None => write!(f, "none"),
-            Self::Keccak256 => write!(f, "keccak256"),
-            Self::Ipfs => write!(f, "ipfs"),
+            Self::IPFS => write!(f, "ipfs"),
         }
     }
 }
